e0b5a71c839dec025eb39e7814e2f0a2e7ed304d
Trinhnt3027 <54887568+Trinhnt3027@users.noreply.github.com>
2022-11-29 12:12:15 +0700

    Update environment.prod.ts

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'ab926fc2b97704f3fbba751ea2863ccc-984385919.us-east-1.elb.amazonaws.com/api/v0'
 };
 
 /*
